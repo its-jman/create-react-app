d14ff1d3c1e234ea2fb26353403c8f35393c3319
Josh Manning <git@jman.me>
2020-10-29 22:10:52 -0500

    add single page styling

--- src/__raw_files__/src/pages/_home.styled.tsx
+++ src/__raw_files__/src/pages/_home.styled.tsx
@@ -1,5 +1,23 @@
-import styled from "styled-components/macro";
+import styled, { createGlobalStyle } from "styled-components/macro";
 
-export const Wrapper = styled.div`
+export const GlobalStyle = createGlobalStyle`
+  html {
+    background-color: var(--grey-0);
+  }
+`;
+
+export const Wrapper = styled.div``;
+
+export const MainContainer = styled.div`
   background-color: var(--blue-8);
+`;
+
+export const Sidebar = styled.div`
+  width: 24px;
+  background-color: var(--grey-8);
+`;
+
+export const Header = styled.div`
+  height: 24px;
+  background-color: var(--grey-9);
 `;
--- src/__raw_files__/src/pages/home.tsx
+++ src/__raw_files__/src/pages/home.tsx
@@ -5,8 +5,23 @@ import { gcn } from "src/utils";
 
 const Home = () => {
   return (
-    <S.Wrapper className={gcn("text-white py-24 text-center text-6xl font-bold")}>
-      Hello, World!
+    <S.Wrapper className={"h-full flex flex-col"}>
+      <S.GlobalStyle />
+
+      <S.Header />
+
+      <div className={"flex-grow flex"}>
+        <S.Sidebar />
+        <div className={"flex-grow"}>
+          <S.MainContainer className={gcn("text-white py-24 text-center text-6xl font-bold")}>
+            Hello, World!
+          </S.MainContainer>
+        </div>
+        <S.Sidebar />
+      </div>
+
+      <S.Header />
+
     </S.Wrapper>
   );
 };
